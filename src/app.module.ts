70e36bc111c719c084ef241c5889ca1aab7fe47a
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 01:13:05 -0500

    moviendo a modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeOrmConfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { typeOrmConfig } from './config/typeOrmConfig';
 import { CoursesModule } from './modules/courses/courses.module';
 import { CampusModule } from './modules/campus/campus.module';
 import { EnrollmentsModule } from './modules/enrollments/enrollments.module';
-import { EnrollmentsCoursesModule } from './enrollments-courses/enrollments-courses.module';
-import { CareersModule } from './careers/careers.module';
-import { CareersLevelsModule } from './careers-levels/careers-levels.module';
-import { AcademicCyclesModule } from './academic-cycles/academic-cycles.module';
-import { CareersEnrollmentsModule } from './careers-enrollments/careers-enrollments.module';
-import { AttendancesModule } from './attendances/attendances.module';
-import { SessionsModule } from './sessions/sessions.module';
-import { SchedulesModule } from './schedules/schedules.module';
+import { EnrollmentsCoursesModule } from './modules/enrollments-courses/enrollments-courses.module';
+import { CareersModule } from './modules/careers/careers.module';
+import { CareersLevelsModule } from './modules/careers-levels/careers-levels.module';
+import { AcademicCyclesModule } from './modules/academic-cycles/academic-cycles.module';
+import { CareersEnrollmentsModule } from './modules/careers-enrollments/careers-enrollments.module';
+import { AttendancesModule } from './modules/attendances/attendances.module';
+import { SessionsModule } from './modules/sessions/sessions.module';
+import { SchedulesModule } from './modules/schedules/schedules.module';
 
 @Module({
   imports: [
