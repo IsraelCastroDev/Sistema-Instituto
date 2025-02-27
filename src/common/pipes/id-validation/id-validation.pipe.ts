5e4a4a20e95c0db79ace8fcccfddd2ab343b097e
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 14:11:56 -0500

    corrigiendo mensaje

--- src/common/pipes/id-validation/id-validation.pipe.ts
+++ src/common/pipes/id-validation/id-validation.pipe.ts
@@ -4,7 +4,7 @@ import { BadRequestException, Injectable, ParseIntPipe } from '@nestjs/common';
 export class IdValidationPipe extends ParseIntPipe {
   constructor() {
     super({
-      exceptionFactory: (error) => new BadRequestException('Invalid ID'),
+      exceptionFactory: (error) => new BadRequestException('ID no válido'),
     });
   }
 }
