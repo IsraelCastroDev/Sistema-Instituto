361fbd3bbce42d651a6e07c65328517d5796eb32
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 14:58:02 -0500

    importando entidades Career y CareersLevel

--- src/modules/careers-levels/careers-levels.module.ts
+++ src/modules/careers-levels/careers-levels.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CareersLevelsService } from './careers-levels.service';
 import { CareersLevelsController } from './careers-levels.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CareersLevel } from './entities/careers-level.entity';
+import { Career } from '../careers/entities/career.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CareersLevel, Career])],
   controllers: [CareersLevelsController],
   providers: [CareersLevelsService],
 })
