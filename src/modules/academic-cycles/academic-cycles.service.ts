327f57d4115dd6590c0860d2ccf59b0af7f31967
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 16:16:49 -0500

    metodo para crear ciclos, con logica para tratar de evitar duplicados

--- src/modules/academic-cycles/academic-cycles.service.ts
+++ src/modules/academic-cycles/academic-cycles.service.ts
@@ -1,11 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateAcademicCycleDto } from './dto/create-academic-cycle.dto';
 import { UpdateAcademicCycleDto } from './dto/update-academic-cycle.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AcademicCycle } from './entities/academic-cycle.entity';
+import { Between, LessThan, MoreThan, Repository } from 'typeorm';
 
 @Injectable()
 export class AcademicCyclesService {
-  create(createAcademicCycleDto: CreateAcademicCycleDto) {
-    return 'This action adds a new academicCycle';
+  constructor(
+    @InjectRepository(AcademicCycle)
+    private readonly academicCycleRepository: Repository<AcademicCycle>,
+  ) {}
+
+  async create(createAcademicCycleDto: CreateAcademicCycleDto) {
+    const { startDate, endDate } = createAcademicCycleDto;
+
+    if (startDate > endDate) {
+      throw new ConflictException(
+        'La fecha de inicio debe ser anterior a la fecha de fin',
+      );
+    }
+
+    const overlappingCycle = await this.academicCycleRepository.findOne({
+      where: [
+        { startDate: Between(startDate, endDate) }, // Si el inicio está dentro de otro ciclo
+        { endDate: Between(startDate, endDate) }, // Si el fin está dentro de otro ciclo
+        { startDate: LessThan(startDate), endDate: MoreThan(endDate) }, // Si el ciclo envuelve completamente otro
+      ],
+    });
+
+    if (overlappingCycle) {
+      throw new ConflictException(
+        'Ya existe un ciclo académico que se superpone con el nuevo ciclo',
+      );
+    }
+
+    const newAcademicCycle = this.academicCycleRepository.create(
+      createAcademicCycleDto,
+    );
+    await this.academicCycleRepository.save(newAcademicCycle);
+
+    return {
+      message: 'Ciclo académico creado exitosamente',
+      academicCycle: newAcademicCycle,
+    };
   }
 
   findAll() {
