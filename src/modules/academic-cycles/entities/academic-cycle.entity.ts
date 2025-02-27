c0ff75ee72ae8a218d2b8f444c0846258636a592
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 12:04:07 -0500

    creando entidad de ciclo aacdemico

--- src/modules/academic-cycles/entities/academic-cycle.entity.ts
+++ src/modules/academic-cycles/entities/academic-cycle.entity.ts
@@ -1,1 +1,21 @@
-export class AcademicCycle {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class AcademicCycle {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({ type: 'date' })
+  startDate: Date;
+
+  @Column({ type: 'date' })
+  endDate: Date;
+
+  @Column({ type: 'boolean' })
+  active: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
