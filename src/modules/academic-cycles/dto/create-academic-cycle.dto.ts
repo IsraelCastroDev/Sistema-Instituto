f747d7300c5e1dd6938cb0bee0f036989b8e629a
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 12:15:53 -0500

    actualizando dtos

--- src/modules/academic-cycles/dto/create-academic-cycle.dto.ts
+++ src/modules/academic-cycles/dto/create-academic-cycle.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAcademicCycleDto {}
+import { IsBoolean, IsDateString, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAcademicCycleDto {
+  @IsNotEmpty({ message: 'El nombre es requerido' })
+  @IsString({ message: 'El nombre debe ser una cadena de texto' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Fecha de inicio es requerida' })
+  @IsDateString({}, { message: 'Fecha de inicio inválida' })
+  startDate: Date;
+
+  @IsNotEmpty({ message: 'Fecha de fin es requerida' })
+  @IsDateString({}, { message: 'Fecha de fin inválida' })
+  endDate: Date;
+
+  @IsNotEmpty({ message: 'Estado es requerido' })
+  @IsBoolean({ message: 'Active debe ser un boolean' })
+  active: boolean;
+}
--- src/modules/courses/dto/create-course.dto.ts
+++ src/modules/courses/dto/create-course.dto.ts
@@ -5,9 +5,15 @@ export class CreateCourseDto {
   @IsString({ message: 'El nombre debe ser una cadena de texto' })
   name: string;
 
+  @IsNotEmpty({ message: 'El userId es requerido' })
   @IsInt({ message: 'El userId debe ser un entero' })
   userId: number;
 
+  @IsNotEmpty({ message: 'El campusId es requerido' })
   @IsInt({ message: 'El campusId debe ser un entero' })
   campusId: number;
+
+  @IsNotEmpty({ message: 'El academicCycleId es requerido' })
+  @IsInt({ message: 'El academicCycleId debe ser un entero' })
+  academicCycleId: number;
 }
