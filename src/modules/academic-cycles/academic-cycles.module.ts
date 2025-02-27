760e64e8cfd71451c9926cb00c52dc250e5fde0d
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 16:16:19 -0500

    importando entidad

--- src/modules/academic-cycles/academic-cycles.module.ts
+++ src/modules/academic-cycles/academic-cycles.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AcademicCyclesService } from './academic-cycles.service';
 import { AcademicCyclesController } from './academic-cycles.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AcademicCycle } from './entities/academic-cycle.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AcademicCycle])],
   controllers: [AcademicCyclesController],
   providers: [AcademicCyclesService],
 })
