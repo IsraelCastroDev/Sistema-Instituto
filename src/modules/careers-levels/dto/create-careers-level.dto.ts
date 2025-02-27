fa6aefe56fbb955aaaca2f3c96afc21314267076
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 12:46:20 -0500

    actualizando ftos

--- src/modules/careers-levels/dto/create-careers-level.dto.ts
+++ src/modules/careers-levels/dto/create-careers-level.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCareersLevelDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateCareersLevelDto {
+  @IsNotEmpty({ message: 'El nombre es requerido' })
+  @IsString({ message: 'El nombre debe ser una cadena de texto' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El ID de la carrera es requerido' })
+  @IsString({ message: 'El ID de la carrera debe ser una cadena de texto' })
+  careerId: string;
+
+  @IsNotEmpty({ message: 'El orden de la carrera es requerido' })
+  @IsInt({ message: 'sequenceOrder debe ser un entero' })
+  sequenceOrder: number;
+
+  @IsOptional()
+  @IsString({ message: 'La descripción debe ser una cadena de texto' })
+  description: string;
+}
--- src/modules/careers/dto/create-career.dto.ts
+++ src/modules/careers/dto/create-career.dto.ts
@@ -1,1 +1,24 @@
-export class CreateCareerDto {}
+import {
+  IsBoolean,
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateCareerDto {
+  @IsNotEmpty({ message: 'El nombre de la carrera es requerido' })
+  @IsString({ message: 'Nombre de carrera inválido' })
+  name: string;
+
+  @IsOptional()
+  @IsString({ message: 'Descripción de carrera inválida' })
+  description?: string;
+
+  @IsBoolean({ message: 'Estado de carrera inválido' })
+  active: boolean;
+
+  @IsOptional()
+  @IsDateString({}, { message: 'Fecha inválida' })
+  createdAt?: Date;
+}
