414f4e71492a0a8ef6f616db842550bf8aacd886
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 15:00:54 -0500

    creando metodo para crear nivel de carrera

--- src/modules/careers-levels/careers-levels.service.ts
+++ src/modules/careers-levels/careers-levels.service.ts
@@ -1,11 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCareersLevelDto } from './dto/create-careers-level.dto';
 import { UpdateCareersLevelDto } from './dto/update-careers-level.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CareersLevel } from './entities/careers-level.entity';
+import { Repository } from 'typeorm';
+import { Career } from '../careers/entities/career.entity';
 
 @Injectable()
 export class CareersLevelsService {
-  create(createCareersLevelDto: CreateCareersLevelDto) {
-    return 'This action adds a new careersLevel';
+  constructor(
+    @InjectRepository(CareersLevel)
+    private careersLevelRepository: Repository<CareersLevel>,
+    @InjectRepository(Career)
+    private careerRepository: Repository<Career>,
+  ) {}
+
+  async create(createCareersLevelDto: CreateCareersLevelDto) {
+    const career = await this.careerRepository.findOne({
+      where: { id: createCareersLevelDto.careerId },
+    });
+
+    if (!career) {
+      throw new NotFoundException('Carrera no encontrada');
+    }
+
+    try {
+      const newCareersLevel = this.careersLevelRepository.create({
+        ...createCareersLevelDto,
+        career,
+      });
+      await this.careersLevelRepository.save(newCareersLevel);
+
+      return {
+        message: 'Nivel de carrera creado exitosamente',
+        career: newCareersLevel,
+      };
+    } catch (error: unknown) {
+      if (error instanceof Error && 'code' in error) {
+        const pgError = error as { code: string; detail?: string };
+
+        if (pgError.code === '23505') {
+          const errorMessage = pgError.detail?.includes('name')
+            ? 'Ya existe un nivel con este nombre en la carrera'
+            : 'El orden de nivel ya está asignado en esta carrera';
+
+          throw new ConflictException(errorMessage);
+        }
+      }
+      throw error; // Re-lanzamos si es otro error
+    }
   }
 
   findAll() {
