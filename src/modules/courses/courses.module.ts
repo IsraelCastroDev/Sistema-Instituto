8a93538724df91204d9e3a3203d34e2037b30e45
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 16:17:24 -0500

    importando entidades

--- src/modules/courses/courses.module.ts
+++ src/modules/courses/courses.module.ts
@@ -5,9 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entities/course.entity';
 import { User } from '../users/entities/user.entity';
 import { Campus } from '../campus/entities/campus.entity';
+import { CareersLevel } from '../careers-levels/entities/careers-level.entity';
+import { AcademicCycle } from '../academic-cycles/entities/academic-cycle.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course, User, Campus])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Course,
+      User,
+      Campus,
+      CareersLevel,
+      AcademicCycle,
+    ]),
+  ],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
