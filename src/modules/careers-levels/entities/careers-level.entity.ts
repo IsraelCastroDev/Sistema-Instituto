d372912b45eb9a8b463c31c4195bf0ed91eecae8
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-02-27 12:45:26 -0500

    creando relacion entre carrera y nivel de carrera

--- src/modules/careers-levels/entities/careers-level.entity.ts
+++ src/modules/careers-levels/entities/careers-level.entity.ts
@@ -1,1 +1,33 @@
-export class CareersLevel {}
+import { Career } from 'src/modules/careers/entities/career.entity';
+import { Course } from 'src/modules/courses/entities/course.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('careersLevel')
+export class CareersLevel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'int' })
+  careerId: number;
+
+  @Column({ type: 'int' })
+  sequenceOrder: number;
+
+  @Column({ type: 'text', nullable: true })
+  description?: string;
+
+  @ManyToOne(() => Career)
+  career: Career;
+
+  @OneToMany(() => Course, (course) => course.careersLevel)
+  courses: Course[];
+}
--- src/modules/careers/entities/career.entity.ts
+++ src/modules/careers/entities/career.entity.ts
@@ -1,1 +1,23 @@
-export class Career {}
+import { CareersLevel } from 'src/modules/careers-levels/entities/careers-level.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('careers')
+export class Career {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  description?: string;
+
+  @Column({ type: 'boolean', default: true })
+  active: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @OneToMany(() => CareersLevel, (careerLevel) => careerLevel.career)
+  careerLevels: CareersLevel[];
+}
