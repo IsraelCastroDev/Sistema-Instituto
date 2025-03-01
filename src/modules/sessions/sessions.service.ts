b67c07a3b252a3abb7c17f5f8ac8d4ac2f043a95
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-01 12:30:32 -0500

    instanciando el servicio de curso

--- src/modules/sessions/sessions.service.ts
+++ src/modules/sessions/sessions.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSessionDto } from './dto/create-session.dto';
 import { UpdateSessionDto } from './dto/update-session.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Session } from './entities/session.entity';
+import { Repository } from 'typeorm';
+import { CoursesService } from '../courses/courses.service';
 
 @Injectable()
 export class SessionsService {
-  create(createSessionDto: CreateSessionDto) {
+  constructor(
+    @InjectRepository(Session)
+    private readonly sessionRepository: Repository<Session>,
+    private courseService: CoursesService,
+  ) {}
+
+  async create(createSessionDto: CreateSessionDto) {
+    const findCourse = await this.courseService.findOne(
+      createSessionDto.courseId,
+    );
     return 'This action adds a new session';
   }
 
