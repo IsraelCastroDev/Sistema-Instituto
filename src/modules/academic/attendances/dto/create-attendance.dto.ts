5cfd89cbfe576e7d6b4b9400cfa8dab1be0a2b96
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-01 15:09:15 -0500

    dto de creacion de asistencia

--- src/modules/academic/attendances/dto/create-attendance.dto.ts
+++ src/modules/academic/attendances/dto/create-attendance.dto.ts
@@ -1,1 +1,24 @@
-export class CreateAttendanceDto {}
+import { IsDate, IsEnum, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { AttendancesStatus } from '../interfaces';
+
+export class CreateAttendanceDto {
+  @IsNotEmpty({ message: 'userId es requerido' })
+  @IsInt({ message: 'userId inválido' })
+  userId: number;
+
+  @IsNotEmpty({ message: 'sessionId es requerido' })
+  @IsInt({ message: 'sessionId inválido' })
+  sessionId: number;
+
+  @IsNotEmpty({ message: 'El estado es requerido' })
+  @IsEnum(AttendancesStatus, {
+    message: `El estado debe ser ${AttendancesStatus.PRESENT},
+    ${AttendancesStatus.ABSENT} o ${AttendancesStatus.LATE}`,
+  })
+  status: AttendancesStatus;
+
+  @IsOptional()
+  @IsNotEmpty({ message: 'createdAt es requerido' })
+  @IsDate({ message: 'Fecha de creación inválida' })
+  createdAt: Date;
+}
