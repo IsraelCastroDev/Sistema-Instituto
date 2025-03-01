e03d06214c0dec7d3a5d8aa50407874d85697002
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-01 15:48:51 -0500

    importando modulos

--- src/modules/academic/attendances/attendances.module.ts
+++ src/modules/academic/attendances/attendances.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AttendancesService } from './attendances.service';
 import { AttendancesController } from './attendances.controller';
+import { UsersModule } from 'modules/user/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Attendance } from './entities/attendance.entity';
+import { SessionsModule } from '../sessions/sessions.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Attendance]),
+    UsersModule,
+    SessionsModule,
+  ],
   controllers: [AttendancesController],
   providers: [AttendancesService],
 })
