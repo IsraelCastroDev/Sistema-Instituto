5dcb8e5d30c1f094eef3a476b493d7b63f58d347
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-01 15:49:07 -0500

    creando metodo para crear asistencia

--- src/modules/academic/attendances/attendances.service.ts
+++ src/modules/academic/attendances/attendances.service.ts
@@ -1,11 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
 import { UpdateAttendanceDto } from './dto/update-attendance.dto';
+import { Attendance } from './entities/attendance.entity';
+import { UsersService } from 'modules/user/users/users.service';
+import { SessionsService } from '../sessions/sessions.service';
+import { AttendancesStatus } from './interfaces';
 
 @Injectable()
 export class AttendancesService {
-  create(createAttendanceDto: CreateAttendanceDto) {
-    return 'This action adds a new attendance';
+  constructor(
+    @InjectRepository(Attendance)
+    private readonly attendanceRepository: Repository<Attendance>,
+    private usersService: UsersService,
+    private sessionsService: SessionsService,
+  ) {}
+
+  async create(createAttendanceDto: CreateAttendanceDto) {
+    const { userId, sessionId } = createAttendanceDto;
+
+    const [user, session] = await Promise.all([
+      this.usersService.findOne(userId),
+      this.sessionsService.findOne(sessionId),
+    ]);
+
+    const findAttendance = await this.attendanceRepository.findOne({
+      where: {
+        userId: user.id,
+        sessionId: session.id,
+      },
+    });
+
+    if (findAttendance) {
+      throw new ConflictException('El estudiante ya marcó su asistencia');
+    }
+
+    const currentTime = new Date();
+    const sessionStartDate = new Date(
+      `${session.sessionDate}T${session.startTime}`,
+    );
+
+    const lateThreshold = new Date(sessionStartDate);
+    lateThreshold.setMinutes(lateThreshold.getMinutes() + 15);
+
+    let status = AttendancesStatus.PRESENT;
+    if (currentTime > lateThreshold) {
+      status = AttendancesStatus.LATE;
+    }
+
+    const newAttendance = this.attendanceRepository.create({
+      userId,
+      sessionId,
+      status,
+    });
+
+    await this.attendanceRepository.save(newAttendance);
+
+    return { message: 'Asistencia marcada' };
   }
 
   findAll() {
