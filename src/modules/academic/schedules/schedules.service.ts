e1b19a8ce27d4b30767e6030ae21ed8cdf8b5ae9
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-01 23:41:03 -0500

    creando metodo para crear un horario, con logica para evitar solapamiento

--- src/modules/academic/schedules/schedules.service.ts
+++ src/modules/academic/schedules/schedules.service.ts
@@ -1,11 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Between, LessThan, MoreThan, Repository } from 'typeorm';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import { Schedule } from './entities/schedule.entity';
+import { CoursesService } from '../courses/courses.service';
+import { AcademicCyclesService } from '../academic-cycles/academic-cycles.service';
+import { CampusService } from 'modules/campus/campus.service';
 
 @Injectable()
 export class SchedulesService {
-  create(createScheduleDto: CreateScheduleDto) {
-    return 'This action adds a new schedule';
+  constructor(
+    @InjectRepository(Schedule)
+    private readonly schedulesRepository: Repository<Schedule>,
+    private coursesService: CoursesService,
+    private academicCyclesService: AcademicCyclesService,
+    private campusService: CampusService,
+  ) {}
+
+  async create(createScheduleDto: CreateScheduleDto) {
+    const {
+      campusId,
+      academicCycleId,
+      courseId,
+      startTime,
+      endTime,
+      dayOfWeek,
+    } = createScheduleDto;
+
+    const [course, academicCycle, campus] = await Promise.all([
+      await this.coursesService.findOne(courseId),
+      await this.academicCyclesService.findOne(academicCycleId),
+      await this.campusService.findOne(campusId),
+    ]);
+
+    const overlappingSchedules = await this.schedulesRepository.count({
+      where: [
+        {
+          courseId,
+          academicCycleId,
+          campusId,
+          dayOfWeek,
+          startTime: Between(startTime, endTime),
+        },
+        {
+          courseId,
+          academicCycleId,
+          campusId,
+          dayOfWeek,
+          endTime: Between(startTime, endTime),
+        },
+        {
+          courseId,
+          academicCycleId,
+          campusId,
+          dayOfWeek,
+          startTime: LessThan(startTime),
+          endTime: MoreThan(endTime),
+        },
+      ],
+    });
+
+    if (overlappingSchedules > 0) {
+      throw new ConflictException('El horario se solapa con otro ya existente');
+    }
+
+    const newSchedule = this.schedulesRepository.create({
+      ...createScheduleDto,
+      campus,
+      course,
+      academicCycle,
+    });
+    await this.schedulesRepository.save(newSchedule);
+
+    return { message: 'Horario creado exitosamente', schedule: newSchedule };
   }
 
   findAll() {
@@ -23,4 +91,9 @@ export class SchedulesService {
   remove(id: number) {
     return `This action removes a #${id} schedule`;
   }
+}
+function GreaterThan(
+  startTime: string,
+): string | import('typeorm').FindOperator<string> | undefined {
+  throw new Error('Function not implemented.');
 }
