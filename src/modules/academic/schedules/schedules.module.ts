903889b1eb7010bc2eb3450ac1bc534b13f3253a
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-01 23:40:37 -0500

    importando modulos

--- src/modules/academic/schedules/schedules.module.ts
+++ src/modules/academic/schedules/schedules.module.ts
@@ -3,9 +3,17 @@ import { SchedulesService } from './schedules.service';
 import { SchedulesController } from './schedules.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Schedule } from './entities/schedule.entity';
+import { CoursesModule } from '../courses/courses.module';
+import { AcademicCyclesModule } from '../academic-cycles/academic-cycles.module';
+import { CampusModule } from 'modules/campus/campus.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Schedule])],
+  imports: [
+    TypeOrmModule.forFeature([Schedule]),
+    CoursesModule,
+    AcademicCyclesModule,
+    CampusModule,
+  ],
   controllers: [SchedulesController],
   providers: [SchedulesService],
 })
